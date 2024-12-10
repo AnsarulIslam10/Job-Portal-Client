dc50803a550b1352b6e5c455221428eab52b1680
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-11 00:04:08 +0600

    footer

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '../pages/shared/Navbar';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../pages/shared/Navbar";
+import Footer from "../pages/shared/Footer";
 
 const MainLayout = () => {
-    return (
-        <div className='max-w-7xl mx-auto px-2'>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div className="max-w-7xl mx-auto px-2">
+      <Navbar></Navbar>
+      <div className="min-h-[calc(100vh-200px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MainLayout;
