61991a3d8fd552b76f43956fefc1fa1f4155e446
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-14 11:47:19 +0600

    banner design

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,29 +1,37 @@
 import React from "react";
 import { motion } from "motion/react";
-import { easeInOut, reverseEasing } from "motion";
+import { easeInOut, easeOut } from "motion";
+import team1 from "../../assets/team/team1.jpg";
+import team2 from "../../assets/team/team2.jpg";
+
 const Banner = () => {
   return (
     <div className="hero min-h-96">
-      <div className="hero-content flex-col lg:flex-row-reverse gap-16">
+      <div className="hero-content grid justify-items-center grid-cols-1 md:grid-cols-2">
         <div>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
-        </div>
-        <div className="max-w-2xl">
-          <motion.h1 
-          animate={{ x: 5}} 
-          transition={{duration: 2, ease: easeInOut, repeat:Infinity}}
-          className="text-5xl font-bold">
-            Latest <motion.span animate={{color: ['blue', "red"], repeat: Infinity}}>Jobs</motion.span> For You!
-          </motion.h1>
-          <p className="py-6">
+          <h1 className="text-5xl font-bold">
+            Latest <span>Jobs</span> For You!
+          </h1>
+          <p className="py-6 max-w-2xl">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
           <button className="btn btn-primary">Get Started</button>
+        </div>
+        <div>
+          <motion.img
+            src={team1}
+            animate={{ y: [50, 100, 50] }}
+            transition={{ duration: 10, repeat: Infinity }}
+            className="max-w-sm w-80 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-400 shadow-2xl"
+          />
+          <motion.img
+            src={team2}
+            animate={{ x: [100, 150, 100] }}
+            transition={{ duration: 10, delay: 5, repeat: Infinity }}
+            className="max-w-sm w-80 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-400 shadow-2xl"
+          />
         </div>
       </div>
     </div>
